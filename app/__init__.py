bb524bdcaeff21ebcbe4fb2a3abbe7f56c2f4163
root <root@Xpc>
2024-10-31 20:55:54 -0400

    change files

---
+++ app/__init__.py
@@ -0,0 +1,59 @@
+import os
+import pkgutil
+import importlib
+import sys
+from app.commands import CommandHandler, Command
+from dotenv import load_dotenv
+import logging
+import logging.config
+
+class App:
+    def __init__(self): # Constructor
+        os.makedirs('logs', exist_ok=True)
+        self.configure_logging()
+        load_dotenv() # Loads the .env file contents
+        self.settings = self.load_environment_variables()
+        self.settings.setdefault('ENVIRONMENT', 'PRODUCTION')
+        self.command_handler = CommandHandler()
+
+    def configure_logging(self):
+        logging_conf_path = 'logging.conf'
+        if os.path.exists(logging_conf_path):
+            logging.config.fileConfig(logging_conf_path, disable_existing_loggers=False)
+        else:
+            logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info("Logging configured.")
+
+    def load_environment_variables(self):
+        settings = {key: value for key, value in os.environ.items()}
+        # print(settings)
+        logging.info("Environment variables loaded.")
+        logging.critical("Oh no I'm broken")
+        return settings
+
+    def get_environment_variable(self, env_var: str = 'ENVIRONMENT'):
+        return self.settings.get(env_var, None)
+
+    def load_plugins(self):
+        # Dynamically load all plugins in the plugins directory
+        plugins_package = 'app.commands'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:  # Ensure it's a package
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
+                            self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue  # If item is not a class or unrelated class, just ignore
+    def start(self):
+        # Register commands here
+        self.load_plugins()
+        logging.info("Application started.")
+        print("Type 'exit' to exit.")
+        while True:
+            command_input = input(">>> ").strip()
+            if command_input.lower() == 'exit':
+                sys.exit("Exiting...")
+            self.command_handler.execute_command(command_input)
---
+++ app/commands/add/__init__.py
@@ -0,0 +1,15 @@
+import sys 
+from app.commands import Command
+
+
+class AddCommand(Command):
+    def execute(self, params=None):
+        if len(params) >= 2:
+            try:
+                a, b = map(float, params)  # Convert both to float to support decimals
+                print(a + b)
+            except valueError:
+                print("please input valid numbers")
+        else:
+            print("please input 2 numbers")
+        print(params)
